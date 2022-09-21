78e19bfca39d23c558d940c27ee5393e2e445364
john-langlois <joao.langlois@hotmail.com>
2022-09-20 22:54:51 -0400

    feat:added js search functionality + alert| added more css

--- Pokedex/filter.js
+++ Pokedex/filter.js
@@ -2,133 +2,162 @@ pokemon = [
     {
         "name":"bulbasaur",
         "id" : "001",
-        "type" : "grass, poison, vine whip"
+        "type" : "grass, poison"
     },
     {
         "name":"ivysaur",
         "id" : "002",
-        "type" : "grass, poison, vine whip"
+        "type" : "grass, poison"
     },
     {
         "name":"venusaur",
         "id" : "003",
-        "type" : "grass, poison, vine whip"
+        "type" : "grass, poison"
     },
     {
         "name":"charmander",
         "id" : "004",
-        "type" : "grass, poison, vine whip"
+        "type" : "Fire"
     },
     {
         "name":"charmeleon",
         "id" : "005",
-        "type" : "grass, poison, vine whip"
+        "type" : "Fire"
     },
     {
         "name":"charizard",
         "id" : "006",
-        "type" : "grass, poison, vine whip"
+        "type" : "Fire, Flying"
     },
     {
         "name":"squirtle",
         "id" : "007",
-        "type" : "grass, poison, vine whip"
+        "type" : "Water"
     },
     {
         "name":"wartortle",
         "id" : "008",
-        "type" : "grass, poison, vine whip"
+        "type" : "Water"
     },
     {
         "name":"blastoise",
         "id" : "009",
-        "type" : "grass, poison, vine whip"
+        "type" : "Water"
     },
     {
         "name":"caterpie",
         "id" : "010",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug"
     },
     {
         "name":"metapod",
         "id" : "011",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug"
     },
     {
         "name":"butterfree",
         "id" : "012",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug, Flying"
     },
     {
         "name":"weedle",
         "id" : "013",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug, Poison"
     },
     {
         "name":"kakuna",
         "id" : "014",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug, Poison"
     },
     {
         "name":"beedrill",
         "id" : "015",
-        "type" : "grass, poison, vine whip"
+        "type" : "Bug, Poison"
     },
     {
         "name":"pidgey",
         "id" : "016",
-        "type" : "grass, poison, vine whip"
+        "type" : "Normal, Flying"
     },
     {
         "name":"pidgeotto",
         "id" : "017",
-        "type" :"grass, poison, vine whip"
+        "type" :"Normal, Flying"
     },
     {
         "name":"pidgeot",
         "id" : "018",
-        "type" : "grass, poison, vine whip"
+        "type" : "Normal, Flying"
     },
     {
         "name":"rattata",
         "id" : "019",
-        "type" : "grass, poison, vine whip"
+        "type" : "Normal"
     },
     {
         "name":"ratticate",
         "id" : "020",
-        "type" : "grass, poison, vine whip"
+        "type" : "Normal"
     },
 ];
 pokemonSearchNumber=[];
 pokemonSearchLetter = [];
 
-let nameInput = document.getElementById("nameInput");
-let submit =  document.getElementById("nameSubmit");
-//submit.addEventListener("click",nameFilter(nameInput));
+//html elements for input field
+let nameInput = document.getElementById("name-input");
+let numberInput = document.getElementById("number-input")
 
+//event listeners for input submission, both click and on enter
+nameInput.addEventListener("keydown",nameEnter);
+numberInput.addEventListener("keydown",numberEnter);
+
+//Letter functionality
+//On enter event
+function nameEnter(event) {
+    if(event.key == "Enter"){
+        nameFilter(nameInput.value);
+        nameInput.value = "";
+    }
+}
+
+//filter through pokemon info, if found, return boolean
 nameFilter = (input) =>{
+    if(!input == null || !input == ""){
     pokemon.forEach(e => {
-        if((e.name.includes(input.toLowerCase())||e.type.includes(input.toLowerCase()))&& pokemonSearchLetter.length<5){
-        pokemonSearchLetter.push("NUMBER:"+e.id + " NAME:"+ e.name+ " ABILITIES: " +e.type);
+        if(((e.name.includes(input.toLowerCase())) && pokemonSearchLetter.length<5)){
+            pokemonSearchLetter.push("NUMBER:"+e.id + " NAME:"+ e.name+ " ABILITIES: " +e.type+ '');
         }
-        else if(input == null || input == ""){
-            alert("Please enter a valid search of letters")
-            }
-        })
-    console.log(pokemonSearchLetter)
-    }
+    })
+    alert(pokemonSearchLetter)
+    pokemonSearchLetter.splice(0,pokemonSearchLetter.length)
+}
+    else{
+        console.log("Enter a valid search")
+    } 
+}
 
-    numberFilter = (input) =>{
-        pokemon.forEach(e => {
-            if((e.id.includes(input.toLowerCase()))&& pokemonSearchNumber.length<5){
+
+//Number Functionality
+//On enter event
+function numberEnter(event) {
+    if(event.key == "Enter"){
+        numberFilter(numberInput.value);
+        numberInput.value = "";
+    }
+}
+
+//filter through pokemon info, if found, return boolean
+numberFilter = (input) =>{
+    if(!input == null || !input == ""){
+    pokemon.forEach(e => {
+        if(((e.id.includes(input.toLowerCase())) && pokemonSearchNumber.length<5)){
             pokemonSearchNumber.push("NUMBER:"+e.id + " NAME:"+ e.name+ " ABILITIES: " +e.type);
-            }
-        })
-        console.log(pokemonSearchNumber)
-    }
-
-    nameFilter("B");
-    numberFilter("3")
-
-    
+        }
+    })
+    alert(pokemonSearchNumber)
+    pokemonSearchNumber.splice(0,pokemonSearchNumber.length)
+}
+    else{
+        console.log("Enter a valid search")
+    } 
+}
