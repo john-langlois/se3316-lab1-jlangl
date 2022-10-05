0bb30e629fb817a8fde4771b9178f0f1de8f7843
john-langlois <joao.langlois@hotmail.com>
2022-10-05 00:08:42 -0400

    feat: adding proper css cards with background when searched

--- Pokedex/filter.js
+++ Pokedex/filter.js
@@ -20,103 +20,103 @@ pokemon = [
     {
         "name":"charmander",
         "id" : "004",
-        "type" : "Fire",
+        "type" : "fire",
         "src": "pokemon/4.png"
     },
     {
         "name":"charmeleon",
         "id" : "005",
-        "type" : "Fire",
+        "type" : "fire",
         "src": "pokemon/5.png"
     },
     {
         "name":"charizard",
         "id" : "006",
-        "type" : "Fire,Flying",
+        "type" : "fire,flying",
         "src": "pokemon/6.png"
     },
     {
         "name":"squirtle",
         "id" : "007",
-        "type" : "Water",
+        "type" : "water",
         "src": "pokemon/7.png"
     },
     {
         "name":"wartortle",
         "id" : "008",
-        "type" : "Water",
+        "type" : "water",
         "src": "pokemon/8.png"
     },
     {
         "name":"blastoise",
         "id" : "009",
-        "type" : "Water",
+        "type" : "water",
         "src": "pokemon/9.png"
     },
     {
         "name":"caterpie",
         "id" : "010",
-        "type" : "Bug",
+        "type" : "bug",
         "src": "pokemon/10.png"
     },
     {
         "name":"metapod",
         "id" : "011",
-        "type" : "Bug",
+        "type" : "bug",
         "src": "pokemon/11.png"
     },
     {
         "name":"butterfree",
         "id" : "012",
-        "type" : "Bug,Flying",
+        "type" : "bug,flying",
         "src": "pokemon/12.png"
     },
     {
         "name":"weedle",
         "id" : "013",
-        "type" : "Bug,Poison",
+        "type" : "bug,poison",
         "src": "pokemon/13.png"
     },
     {
         "name":"kakuna",
         "id" : "014",
-        "type" : "Bug,Poison",
+        "type" : "bug,poison",
         "src": "pokemon/14.png"
     },
     {
         "name":"beedrill",
         "id" : "015",
-        "type" : "Bug,Poison",
+        "type" : "bug,poison",
         "src": "pokemon/15.png"
     },
     {
         "name":"pidgey",
         "id" : "016",
-        "type" : "Normal,Flying",
+        "type" : "normal,flying",
         "src": "pokemon/16.png"
     },
     {
         "name":"pidgeotto",
         "id" : "017",
-        "type" :"Normal,Flying",
+        "type" :"normal,flying",
         "src": "pokemon/17.png"
     },
     {
         "name":"pidgeot",
         "id" : "018",
-        "type" : "Normal,Flying",
+        "type" : "normal,flying",
         "src": "pokemon/18.png"
     },
     {
         "name":"rattata",
         "id" : "019",
-        "type" : "Normal",
+        "type" : "normal",
         "src": "pokemon/19.png"
     },
     {
         "name":"ratticate",
         "id" : "020",
-        "type" : "Normal",
+        "type" : "normal",
         "src": "pokemon/20.png"
     },
 ];
@@ -134,6 +134,11 @@ numberInput.addEventListener("keyup",numberEnter);
 //Letter functionality
 //On enter event
 function nameEnter() {
+    let child = searchResult.lastElementChild;
+  while (child) {
+    searchResult.removeChild(child);
+    child = searchResult.lastElementChild;
+  }
     if(!nameInput.value == ""){
         nameFilter(nameInput.value);
     }
@@ -180,6 +185,7 @@ numberFilter = (input) =>{
             let typeArr = e.type.split(',')
             console.log(typeArr)
             cardDisplay(e.src,e.name,e.id,typeArr[0],typeArr[1])
+            
         }
     })
 
@@ -202,35 +208,40 @@ function numberCheck(cond){
 
 //creating each component that makes up the card component 
 function cardDisplay(img_src, pokemon_name, pokemon_id, type1, type2){
-    let card = document.createElement("li");
-    let card_article =  document.createElement("article")
-    card.appendChild(card_article)
+        var card = document.createElement("li");
+        card.id = 'dynamic-ul'
+        let card_article =  document.createElement("article")
+        card.appendChild(card_article)
 
-    let card_image = document.createElement("img")
-    card_image.src = img_src
-    card_image.alt = ""
-    card_article.appendChild(card_image)
+        card_article.classList.add('bg-search')
 
-    let card_text = document.createElement("div")
-    let text_title = document.createElement("h3")
-    let text_id = document.createElement("h2")
-    let btn1 = document.createElement("btn")
-    let btn2 = document.createElement("btn")
+        let card_image = document.createElement("img")
+        card_image.src = img_src
+        card_image.alt = ""
+        card_article.appendChild(card_image)
+
+        let card_text = document.createElement("div")
+        let text_title = document.createElement("h3")
+        let text_id = document.createElement("h2")
+        let btn1 = document.createElement("btn")
+        let btn2 = document.createElement("btn")
+
+        card_text.classList.add('text')
+        btn1.classList.add(type1)
+        btn2.classList.add(type2)
     
-    card_article.appendChild(card_text)
-    card_text.appendChild(text_title)
-    card_text.appendChild(text_id)
-    card_text.appendChild(btn1)
-    card_text.appendChild(btn2)
+        card_article.appendChild(card_text)
+        card_text.appendChild(text_title)
+        card_text.appendChild(text_id)
+        card_text.appendChild(btn1)
+        card_text.appendChild(btn2)
 
-    card_text.classList.add('text')
-    btn1.classList.add(type1)
-    btn2.classList.add(type2)
+        
 
-    text_title.innerText = pokemon_name
-    text_id.innerText = pokemon_id
-    btn1.innerText = type1
-    btn2.innerText = type2
+        text_title.innerText = pokemon_name
+        text_id.innerText = pokemon_id
+        btn1.innerText = type1
+        btn2.innerText = type2
 
-    document.getElementById("search-results").appendChild(card)
+        document.getElementById("search-results").appendChild(card)
 }
